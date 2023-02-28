251b7b89b4aabdec7a586faccb3132b0c2ad1405
Danielloas <114517047+Danielloas@users.noreply.github.com>
2023-02-28 18:46:09 +0300

    Initial commit

---
+++ MyMonitoring.DEL/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMonitoring.Domain.Entity;
+
+namespace MyMonitoring.DEL
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            Database.EnsureDeleted();
+        }
+        public DbSet<Car> Car { get; set; }
+    }
+}
---
+++ MyMonitoring.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyMonitoring.Domain
+{
+    public class Class1
+    {
+    }
+}
